b0309704dc64500dff676d27c6e9b10aea56e618
Jinwhl <103013505+Jinwhl@users.noreply.github.com>
2024-05-08 09:26:32 +0900

    Update Assignment0507.kt
    Add Feedback

--- Assignment0507.kt
+++ Assignment0507.kt
@@ -3,7 +3,7 @@ fun main(){
     var sum = 0 // sum = 0 으로 초기화
     
     for (i in 1..100){
-        if (i % 2 != 0) continue // 홀수인 case에 대해서 아무런 동작하지 않고 아래 실행
+        if (i % 2 != 0) continue // i = 홀수 case는 생략하고 아래 실행
         sum += i // 짝수일 경우 i 값을 sum 에 누적
     }
     
@@ -26,10 +26,16 @@ fun main(){
     var n = 15
     
     for (i in 1..n){
-        if (i % 2 == 0) continue // 짝수인 case에 대해서 아무런 동작하지 않고 아래 실행
+        if (i % 2 == 0) continue // i = 짝수 case는 생략하고 아래 실행
         for (j in 1..i){
             print('*')
         }
         println()
     }
 }
+
+/* i = 홀수 case 또는 짝수 case를 생략하지 않고
+ * for (i in 1..100) {if (i % 2 == 0) {sum += i}} 처럼 작성할수도 있지만
+ * continue 사용해보고 싶어서 위와 같이 작성했음
+ * 주석의 중요함을 느낌.
+ * */
